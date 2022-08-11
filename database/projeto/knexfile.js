a1433a1bbc1f741f2992c9489a150ada43d2e233
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-08-10 22:20:44 -0300

    chore: Added migrations.

---
+++ database/projeto/knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+require('dotenv/config')
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  client: 'mysql',
+  connection: {
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
